8dbcdcd239b22b5503e8721df41f436e67d34d59
egor <porem.578@gmail.com>
2026-07-25 01:33:20 +0700

    updated config_reader

--- config_reader.py
+++ config_reader.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import SecretStr
 
 class Config(BaseSettings):
-    bot_token: SecretStr
+    BOT_TOKEN: SecretStr
     model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
 
 config = Config()
