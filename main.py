d7c234af72e09b09d0d08ff144ef1ac12aaf86ba
egor <porem.578@gmail.com>
2026-07-25 01:16:42 +0700

    added bot configuration

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
-from aiogram import Dispatcher, Bot
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher
 from config_reader import config
+from handlers import common
 
-bot = Bot(token=config.TOKEN)
-dispatcher = Dispatcher()
+logging.basicConfig(level=logging.INFO)
 
 
-if __name__ == '__main__':
-    ...
+async def main():
+    bot = Bot(token=config.bot_token.get_secret_value())
+
+    dp = Dispatcher()
+
+    dp.include_router(common.router)
+
+    await bot.delete_webhook(drop_pending_updates=True)
+
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
