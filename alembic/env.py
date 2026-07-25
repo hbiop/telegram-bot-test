c7297a209b4062c7e8fba77028ff8998297bd1c3
egor <porem.578@gmail.com>
2026-07-26 02:26:58 +0700

    changed alembic configuration

--- alembic/env.py
+++ alembic/env.py
@@ -1,14 +1,12 @@
 import asyncio
 from logging.config import fileConfig
-
+from db.db_utils.base import Base, User, Order
 from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
 
-from db.db_utils.base import Base
-target_metadata = Base.metadata
 
 from config_reader import config as pydantic_config
 
@@ -25,7 +23,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
