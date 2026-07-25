6aaa38d70b5ac61cd25a01993f3f729288d2d139
egor <porem.578@gmail.com>
2026-07-25 22:11:48 +0700

    redis include to the project

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 import asyncio
 import logging
 from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.redis import RedisStorage
+from redis.asyncio import Redis
 from config_reader import config
-from handlers import common, menu
+from handlers import common, menu, form
 
 logging.basicConfig(level=logging.INFO)
 
 
 async def main():
+    redis_instance = Redis.from_url("redis://localhost:6379/0")
+
+    storage = RedisStorage(redis=redis_instance)
+
     bot = Bot(token=config.BOT_TOKEN.get_secret_value())
 
-    dp = Dispatcher()
+    dp = Dispatcher(storage=storage)
 
     dp.include_router(common.router)
     dp.include_router(menu.router)
+    dp.include_router(form.router)
 
     await bot.delete_webhook(drop_pending_updates=True)
 
